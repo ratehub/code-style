ac8db77b8bae02eee9a1bd7e5ecb81080505ccd7
Chris Skoblenick <chris.skoblenick@ratehub.ca>
2018-05-16 15:46:35 -0400

    created repository for javascript

---
+++ javascript/rules/import.js
@@ -0,0 +1,47 @@
+module.exports = {
+
+   "plugins": [
+
+   ],
+
+   "rules": {
+
+      // Errors
+      "import/no-unresolved": "error",
+      "import/named": "error",
+      "import/default": "error",
+      "import/namespace": "error",
+      "import/no-restricted-paths": "error",
+      "import/no-absolute-path": "error",
+      "import/no-internal-modules": "error",
+      "import/no-webpack-loader-syntax": "error",
+      "import/no-self-import": "error",
+      "import/no-cycle": "error",
+      "import/export": "error",
+      "import/no-named-as-default": "error",
+      "import/no-named-as-default-member": "error",
+      "import/no-depreciated": "error",
+      "import/no-extraneous-dependencies": "error",
+      "import/no-mutable-exports": "error",
+      "import/unambiguous": "error",
+      "import/no-commonjs": "error",
+      "import/no-amd": "error",
+      "import/no-nodejs-modules": "error",
+      "import/first": "error",
+      "import/exports-last": "error",
+      "import/no-duplicates": "error",
+      "import/no-namespace": "error",
+      "import/extensions": "error",
+      "import/order": "error",
+      "import/newline-after-import": "error",
+      "import/prefer-default-export": "error",
+      "import/no-unassigned-import": "error",
+      "import/no-named-default": "error",
+      "import/no-anonymous-default-export": "error",
+      "import/group-exports": "error",
+      
+      // Warnings
+
+
+   }
+}
