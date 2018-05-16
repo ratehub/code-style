c8b8874505c610a3d3f76f2e36675b8212baddb3
Chris Skoblenick <chris.skoblenick@ratehub.ca>
2018-05-16 15:58:43 -0400

    filled-in rules to match README

--- javascript/rules/import.js
+++ javascript/rules/import.js
@@ -1,47 +1,56 @@
 module.exports = {
 
    "plugins": [
-
+      "import"
    ],
 
    "rules": {
 
-      // Errors
+      // Code Smells - Errors
       "import/no-unresolved": "error",
       "import/named": "error",
       "import/default": "error",
       "import/namespace": "error",
-      "import/no-restricted-paths": "error",
       "import/no-absolute-path": "error",
-      "import/no-internal-modules": "error",
+      "import/no-dynamic-require": "error",
       "import/no-webpack-loader-syntax": "error",
       "import/no-self-import": "error",
       "import/no-cycle": "error",
-      "import/export": "error",
       "import/no-named-as-default": "error",
       "import/no-named-as-default-member": "error",
       "import/no-depreciated": "error",
       "import/no-extraneous-dependencies": "error",
       "import/no-mutable-exports": "error",
-      "import/unambiguous": "error",
+      "import/no-namespace": "error",
+
+      // Code Smells - Warnings
+      "import/no-internal-modules": "warn",
+
+      // Code Smells - Parking Lot
+      "import/no-restricted-paths": "off",
+      "import/no-depreciated": "off",
+      "import/unambiguous": "off",
+
+
+      // Readability - Errors
+      "import/export": "error",      
       "import/no-commonjs": "error",
       "import/no-amd": "error",
       "import/no-nodejs-modules": "error",
       "import/first": "error",
       "import/exports-last": "error",
-      "import/no-duplicates": "error",
-      "import/no-namespace": "error",
-      "import/extensions": "error",
       "import/order": "error",
-      "import/newline-after-import": "error",
-      "import/prefer-default-export": "error",
+      "import/newline-after-import": ["error", 2],
       "import/no-unassigned-import": "error",
       "import/no-named-default": "error",
-      "import/no-anonymous-default-export": "error",
       "import/group-exports": "error",
       
-      // Warnings
-
-
+      // Readability - Warnings
+      "import/prefer-default-export": "warn",
+      
+      // Readability - Parking Lot
+      "import/no-duplicates": "off",
+      "import/no-anonymous-default-export": "off",
+      "import/extensions": "off"
    }
 }
