f274d3ca35b7033e1fcca1541a61ea2aa4d12feb
Chris Skoblenick <chris.skoblenick@ratehub.ca>
2019-11-27 09:47:02 -0500

    temporary comment out spacing adjustments

--- javascript/rules/base.js
+++ javascript/rules/base.js
@@ -12,15 +12,17 @@ module.exports = {
         "array-bracket-spacing": ["error", "always"],
         "object-curly-spacing": ["error", "always"],
 
-        "quotes": ["error", "single"],
-        "jsx-quotes": ["error", "prefer-double"],
+        // [Nov 27, 2019] @chris.skoblenick: commented out the quotes enforecement for now; 
+        // REASON: will cause massive monorepo changes; need to incorporate once most PRs merged.
+        // "quotes": ["error", "single"],
+        // "jsx-quotes": ["error", "prefer-double"],
 
-        //   "for-direction" : "off",
-        //   "getter-return" : "error",
-        //   "no-await-in-loop" : "warn",
-        //   "no-compare-neg-zero" : "error",
+        // "for-direction" : "off",
+        // "getter-return" : "error",
+        // "no-await-in-loop" : "warn",
+        // "no-compare-neg-zero" : "error",
 
-        //   "accessor-pairs" : ["error", {"setWithoutGet" : true, "getWithoutSet" : false}], 
+        // "accessor-pairs" : ["error", {"setWithoutGet" : true, "getWithoutSet" : false}], 
     }
 
 }
