c286afba58a9f7c729cd03b28807254708057a7d
Josh Lalonde <mori.ryuuji3@gmail.com>
2022-07-25 12:56:41 -0300

    refactor: address pr comments

--- react/rules/jsx-control-statements.js
+++ react/rules/jsx-control-statements.js
@@ -5,6 +5,20 @@ module.exports = {
   "extends": [
     "plugin:jsx-control-statements/recommended"
   ],
+  // If these are not added, react/jsx-no-undef will find these are undefined.
+  // Ideally this block can be removed if a cleaner solution is found.
+  "globals": {
+    "If": false,
+    "Else": false,
+
+    "For": false,
+
+    "Choose": false,
+    "When": false,
+    "Otherwise": false,
+
+    "With": false
+  },
   "rules": {
     // Required for 7.0.0+ to avoid errors for If/Choose/etc.
     "react/jsx-no-undef": ["error", { "allowGlobals": true }]
