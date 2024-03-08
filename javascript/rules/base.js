4d845e2fe3bf4044c41f36fa95ebee1000df91ac
Ray <97109156+ray1444@users.noreply.github.com>
2024-03-08 16:14:39 -0500

    chore(javascript): Migrated eslint to use stylistic rules (#43)
    * chore(javascript): Added max-params and no-shadow eslint rule
    
    * chore(javascript): Migrated eslint to use stylistic rules
    
    * moved dependency

--- javascript/rules/base.js
+++ javascript/rules/base.js
@@ -1,30 +1,17 @@
 module.exports = {
-    "extends": [
-        "eslint:recommended"
+    'plugins': [
+        '@stylistic/js',
     ],
 
-    "rules": {
-        "indent": ["error", 4, { "SwitchCase": 1 }],
-        "no-console": ["warn"],
-        "keyword-spacing": ["error", { "before": true, "after": true }],
-        "array-bracket-spacing": ["error", "always"],
-        "object-curly-spacing": ["error", "always"],
-        "comma-dangle": ["warn", ["warn", {
-            "arrays": "always-multiline",
-            "objects": "always-multiline",
-            "imports": "always-multiline",
-            "exports": "always-multiline",
-            "functions": "only-multiline",
-         }]],
-        "no-unused-vars": ["error", { "ignoreRestSiblings": true }],
-        "max-params": ["error"],
-        "brace-style": ["error", "1tbs"],
-    
-        "quotes": ["error", "single"],
-        "no-multi-spaces": ["error", { "ignoreEOLComments": true }],
-        "no-shadow": ["error"],
+    'extends': [
+        'eslint:recommended',
+    ],
 
-        "multiline-ternary": ["error", "always-multiline"],
-        "operator-linebreak": ["warn", "before"],       // This will be later made an error. Done this way to make migration easier.
-    }
+    'rules': {
+        'no-console': [ 'warn' ],
+        'no-unused-vars': [ 'error', { 'ignoreRestSiblings': true } ],
+        'max-params': [ 'error' ],
+    
+        'no-shadow': [ 'error' ],
+    },
 }
--- javascript/rules/import.js
+++ javascript/rules/import.js
@@ -1,65 +1,64 @@
 module.exports = {
 
-   "plugins": [
-      "import"
-   ],
+    'plugins': [
+        'import',
+    ],
 
-   "settings": {
-     // Failure to add this makes this check take an eternity
-     "import/ignore": [
-       "node_modules"
-      ]
-   },
+    'settings': {
+        // Failure to add this makes this check take an eternity
+        'import/ignore': [
+            'node_modules',
+        ],
+    },
  
-   "rules": {
-      // Code Smells - Errors
-      "import/no-unresolved": "error",
-      "import/named": "error",
-      "import/default": "error",
-      "import/namespace": "error",
-      "import/no-absolute-path": "error",
-      "import/no-dynamic-require": "error",
-      "import/no-webpack-loader-syntax": "error",
-      "import/no-self-import": "error",
-      "import/no-cycle": "error",
-      "import/no-named-as-default": "error",
-      "import/no-named-as-default-member": "error",
-      "import/no-depreciated": "error",
-      "import/no-extraneous-dependencies": "error",
-      "import/no-mutable-exports": "error",
-      "import/no-namespace": "error",
+    'rules': {
+        // Code Smells - Errors
+        'import/no-unresolved': 'error',
+        'import/named': 'error',
+        'import/default': 'error',
+        'import/namespace': 'error',
+        'import/no-absolute-path': 'error',
+        'import/no-dynamic-require': 'error',
+        'import/no-webpack-loader-syntax': 'error',
+        'import/no-self-import': 'error',
+        'import/no-cycle': 'error',
+        'import/no-named-as-default': 'error',
+        'import/no-named-as-default-member': 'error',
+        'import/no-extraneous-dependencies': 'error',
+        'import/no-mutable-exports': 'error',
+        'import/no-namespace': 'error',
 
-      // Code Smells - Warnings
+        // Code Smells - Warnings
 
-      // Code Smells - Parking Lot
-      "import/no-restricted-paths": "off",
-      "import/no-depreciated": "off",
-      "import/unambiguous": "off",
+        // Code Smells - Parking Lot
+        'import/no-restricted-paths': 'off',
+        'import/no-deprecated': 'off',
+        'import/unambiguous': 'off',
 
 
-      // Readability - Errors
-      "import/export": "error",      
-      "import/no-commonjs": "error",
-      "import/no-amd": "error",
-      "import/no-nodejs-modules": "error",
-      "import/first": "error",
-      "import/exports-last": "error",
-      "import/order": "error",
-      "import/newline-after-import": ["error", {"count": 2}],
-      "import/no-unassigned-import": "error",
-      "import/no-named-default": "error",
+        // Readability - Errors
+        'import/export': 'error',      
+        'import/no-commonjs': 'error',
+        'import/no-amd': 'error',
+        'import/no-nodejs-modules': 'error',
+        'import/first': 'error',
+        'import/exports-last': 'error',
+        'import/order': 'error',
+        'import/newline-after-import': [ 'error', { 'count': 2 } ],
+        'import/no-unassigned-import': 'error',
+        'import/no-named-default': 'error',
       
-      // Readability - Warnings
-      "import/prefer-default-export": "warn",
+        // Readability - Warnings
+        'import/prefer-default-export': 'warn',
       
-      // Readability - Parking Lot
-      "import/no-duplicates": "off",
-      "import/no-anonymous-default-export": "off",
-      "import/extensions": "off",
+        // Readability - Parking Lot
+        'import/no-duplicates': 'off',
+        'import/no-anonymous-default-export': 'off',
+        'import/extensions': 'off',
 
 
-      // Disabled
-      "import/no-internal-modules": "off",
-      "import/group-exports": "off"
-   }
+        // Disabled
+        'import/no-internal-modules': 'off',
+        'import/group-exports': 'off',
+    },
 }
